2399dc2cfb58b353c1580ad08c0d9ebc36c15278
Thiago soares <104157930+0thiago@users.noreply.github.com>
2022-10-13 10:23:58 -0300

    initial backend structure

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+
+const db = require('./database/db')
+// const routes = require('./routes/routes')
+
+const app = express()
+
+db.connect()
+
+const allowedOrigins = [
+  'http://127.0.0.1:5500',
+]
+
+app.use(cors({
+  origin: function(origin, callback) {
+    let allowed = true
+
+    if (!origin) allowed = true
+
+    if (!allowedOrigins.includes(origin)) allowed = false
+
+    callback(null, allowed)
+  }
+}))
+
+app.use(express.json())
+
+app.use('/api', routes)
+
+const port = proccess.env.PORT || 8080
+app.listen(port, () => {
+  console.log('The server is running on port: ' + port)
+})
