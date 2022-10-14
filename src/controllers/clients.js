973370aac09db31d6e8db3d3e1fcbb2932e2ea75
Thiago soares <104157930+0thiago@users.noreply.github.com>
2022-10-13 21:36:26 -0300

    adding clients post route and post controller

--- src/controllers/clients.js
+++ src/controllers/clients.js
@@ -10,6 +10,29 @@ async function get(req, res) {
   res.send(clients)
 }
 
+async function post(req, res) {
+  const {
+    name,
+    email,
+    phone,
+    address,
+  } = req.body
+
+  const clients = new ClientsModel({
+    name,
+    email,
+    phone,
+    address,
+  })
+
+  clients.save()
+
+  res.send({
+    message: 'success'
+  })  
+}
+
 module.exports = {
   get,
+  post,
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,4 +4,6 @@ const ClientsController = require('../controllers/clients')
 
 router.get('/clients/:id?', ClientsController.get)
 
+router.post('/clients', ClientsController.post)
+
 module.exports = router
