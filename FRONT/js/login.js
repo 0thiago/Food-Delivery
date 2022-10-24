4e3b21aac61ba86b0afee6c9ee4f32d400dc2cc7
Thiago soares <104157930+0thiago@users.noreply.github.com>
2022-10-23 22:37:44 -0300

    mobile portrait login page finished

--- FRONT/js/login.js
+++ FRONT/js/login.js
@@ -1,30 +1,73 @@
 import {
-  toggleDropMenu, 
+  toggleDropMenu,
   authLogin,
 } from './helpers.js'
 
 toggleDropMenu()
 
 function formLogin() {
-  const form = document.querySelector('#loginForm')
-  const usernameInput = document.querySelector('#username')
-  const passwordInput = document.querySelector('#password')
-  const loginButton = document.querySelector('#loginButton')
+  
+  const $usernameInput = document.querySelector('#username')
+  const $passwordInput = document.querySelector('#password')
+  const $loginButton = document.querySelector('#loginButton')
 
-  loginButton.addEventListener("click", (button) => {
-    button.preventDefault()
+  $loginButton.addEventListener("click", (button) => {
 
-    const username = usernameInput
-    const password = passwordInput
+    const username = $usernameInput
+    const password = $passwordInput
 
-    let result = authLogin(username, password)
-
-    if (result) {
-      console.log(result)//form SUBMIT
-    } else {
-      console.log(result)
+    if (!authLogin(username, password)) {
+      return false
     }
   })
 }
 
 formLogin()
+
+const $form = document.querySelector('#loginForm')
+
+$form.onsubmit = function(event) {
+  event.preventDefault()  
+
+  const API_URL = 'http://192.168.1.5:8080/api/login'
+
+  const $incorrectUserOrPass = document.querySelector('#IncorrectUserOrPass')
+
+  const username = document.forms['loginForm'].username.value
+
+  const password = document.forms['loginForm'].password.value
+
+  const header = {
+    method: 'POST',
+    body: JSON.stringify({
+      username,
+      password
+    }),
+    headers: { 'Content-Type': 'application/json '}
+  }
+
+  fetch(API_URL, header).then(response => {
+    response.json().then(data => {
+      if(data.message === 'username or password incorrect') {
+        $incorrectUserOrPass.style.opacity = '1'  
+        return false
+      } else {
+
+        const userToken = [
+          {user: data.name},
+          {token: data.token}        
+        ]
+        const tokenJSON = JSON.stringify(userToken)
+        localStorage.setItem('token', tokenJSON)
+
+        alert(`Welcome ${data.name}!`)
+
+        window.location.href = '/index.html'
+
+      }
+    })
+  })
+
+}
+
+
