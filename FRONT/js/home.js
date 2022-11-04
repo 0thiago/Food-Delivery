0acabc5c51fdebf5f96aac1b58a72bea29efd298
Thiago soares <104157930+0thiago@users.noreply.github.com>
2022-11-04 06:41:15 -0300

    recommiting

--- FRONT/js/home.js
+++ FRONT/js/home.js
@@ -1,14 +1,17 @@
 import {
   productsCarousel,
   getSalesProducts,
+  showProductsAmountOnCart,
   toggleDropMenu,
   openCartPage,
 } from './helpers.js'
 
 openCartPage()
+showProductsAmountOnCart()
 productsCarousel()
 getSalesProducts()
 toggleDropMenu()
+
 
 
 
